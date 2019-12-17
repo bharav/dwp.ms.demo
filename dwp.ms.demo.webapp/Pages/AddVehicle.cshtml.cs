55e95ebf4d9079ac2837f1720e270bbf22501787
Kuzhikali, Jithesh <Jithesh.Kuzhikali@emc.com>
2019-12-17 12:29:57 +0530

    Added UI for vehicle add/display

---
+++ dwp.ms.demo.webapp/Pages/AddVehicle.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using dwp.ms.demo.webapp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace dwp.ms.demo.webapp.Pages
+{
+    public class AddVehicleModel : PageModel
+    {
+        public IConfiguration _configuration;
+        public AddVehicleModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public void OnGet()
+        {
+
+        }
+
+        [BindProperty]
+        public Vehicle Vehicle { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            HttpClient myClnt = new HttpClient();
+            var URL = _configuration["ApiURLVehicle"];
+            string stringData = JsonConvert.SerializeObject(Vehicle);
+            var contentData = new StringContent(stringData, System.Text.Encoding.UTF8,
+                                   "application/json");
+            HttpResponseMessage response = myClnt.PostAsync(URL, contentData).Result;
+            var Message = response.Content.
+            ReadAsStringAsync().Result;
+            return RedirectToPage("./Vehicle");
+        }
+    }
+}
